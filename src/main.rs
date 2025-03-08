d457a96a16415bd703d9bbb4860b851921bb3ced
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2025-03-08 08:29:30 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut ram: [u8; 4096];
+    let mut pc: u8;
+    let mut index_register: u16;
+    let mut stack: Vec<u16>;
+    let mut delay_timer: u8;
+    let mut sound_timer: u8;
+    let mut registers: [u8; 16];
+}
