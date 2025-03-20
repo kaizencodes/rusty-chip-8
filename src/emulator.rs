671998ba3b7342a3cf470f20bb62b676cfbb65f7
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2025-03-20 22:02:28 +0000

    implements basic instructions for ibm rom

--- src/emulator.rs
+++ src/emulator.rs
@@ -16,41 +16,125 @@ type Instruction = u16;
 
 pub fn run(rom: String, input_rx: Receiver<Key>, output_tx: Sender<window::DisplayBuffer>) {
     let mut memory: Memory = [0; MEMORY_SIZE];
-    let mut pc: usize = 200;
-    let mut index_register: u16;
+    let mut pc: usize = PROGRAM_START;
+    let mut index_register: u16 = 0x0;
     let mut stack: Vec<u16>;
     let mut delay_timer: u8;
     let mut sound_timer: u8;
-    let mut registers: [u8; 16];
+    let mut registers: [u8; 16] = [0x0; 16];
     
     let file: File = File::open(rom).expect("Rom could not be opened.");
     load_fonts(&mut memory);
     load_program(&mut memory, file);
 
     println!("{:02X?}", memory);
+    let mut display_buffer =[0 as u32; window::WIDTH * window::HEIGHT];
 
-    loop {
-        while let Ok(key) = input_rx.try_recv() {
-            let mut display_buffer =[0 as u32; window::WIDTH * window::HEIGHT];
-            // placeholder to test the setup
-            // println!("key {:?} pressed", key as usize);
-            display_buffer[key as usize] ^= 0xFFFFFF;
+    loop {                
+        let instruction = fetch(&mut pc, &memory);
+        let op_code = (instruction >> 12) & 0xF;
+        let register_x = ((instruction >> 8) & 0xF) as u8;
+        let register_y = ((instruction >> 4) & 0xF) as u8;
+        let address = instruction & 0xFFF;
+        let value = (instruction & 0xFF) as u8;
+        let nibble = (instruction & 0xF) as u8;
 
-            output_tx.send(display_buffer.clone()).unwrap();
+        match op_code {
+            0x0 => { clear_screen(&mut display_buffer, &output_tx) },
+            0x1 => {
+                println!("inst: {:04X}, jump, pc before {:02X}", instruction, &pc);
+                jump(address, &mut pc);
+                println!("pc after {:02X}", &pc);
+            },
+            0x6 => {
+                println!("inst: {:04X}, set registers before {:?}", instruction, &registers);
+                set_register(&mut registers, register_x, value);
+                println!("registers after {:?}", &registers);
+            },
+            0x7 => {
+                println!("inst: {:04X}, add registers before {:?}", instruction, &registers);
+                add_to_register(&mut registers, register_x, value);
+                println!("registers after {:?}", &registers);
+            },
+            0xA => {
+                println!("inst: {:04X}, set index before {:?}", instruction, &index_register);
+                set_index(&mut index_register, address);
+                println!("index after {:?}", &index_register);
+            },
+            0xD => {
+                println!("inst: {:04X}, display", instruction);
+                display(&mut registers, register_x, register_y, nibble, &mut display_buffer, &index_register, &memory, &output_tx);
+            },
+            _ => {}
         }
-        
-        // fetch
-        // decode
-        // execute
 
         sleep(SLEEP_DURATION);
     }
 }
 
-fn fetch(pc: &mut usize, ram: &Memory) -> Instruction {
-    let inst = u16::from_be_bytes([ram[*pc], ram[*pc + 1]]);
-    *pc += 2;
+fn fetch(pc: &mut usize, memory: &Memory) -> Instruction {
+    let inst = u16::from_be_bytes([memory[*pc], memory[*pc + 1]]);
+    *pc += 0x02;
     inst
+}
+
+fn clear_screen(buffer: &mut [u32; 2048], output_tx: &Sender<window::DisplayBuffer>) {
+    *buffer = [0 as u32; window::WIDTH * window::HEIGHT];
+    output_tx.send(buffer.clone()).unwrap();
+}
+
+fn jump(address: u16, pc: &mut usize) {
+    *pc = address as usize;
+}
+
+fn set_register(registers: &mut [u8; 16], register: u8, value: u8) {
+    registers[register as usize] = value
+}
+
+fn add_to_register(registers: &mut [u8; 16], register: u8, value: u8) {
+    registers[register as usize] += value
+}
+
+fn set_index(index_register: &mut u16, address: u16) {
+    *index_register = address;
+}
+
+fn display(registers: &mut [u8; 16], vx: u8, vy: u8, size: u8, buffer: &mut [u32; 2048], index_register: &u16, memory: &Memory, output_tx: &Sender<window::DisplayBuffer>) {
+    let x = registers[vx as usize] & (window::WIDTH - 1) as u8;
+    let y = registers[vy as usize] & (window::HEIGHT - 1) as u8;
+    println!("coords {}, {}", x, y);
+
+    registers[0xF] = 0;
+    for i in 0..size {
+        if y + i >= window::HEIGHT as u8 {
+            break;
+        }
+
+        let sprite_data = memory[*index_register as usize + i as usize];
+        println!("sprite_data {:08b}", sprite_data);
+        for xi in 0..8 {
+            if x + xi >= window::WIDTH as u8 {
+                break;
+            }
+            
+            let current_bit = sprite_data >> (7 - xi) & 0x1;
+            println!("current_bit {}", current_bit);
+            if current_bit == 0x0 {
+                continue;
+            }
+            
+            let current_pixel = (y+i) as usize * window::HEIGHT + (x+xi) as usize;
+            println!("current_pixel {}", current_pixel);
+
+            if buffer[current_pixel] == 0xFFFFFF {
+                registers[0xF] = 0x1;
+            }
+            
+            buffer[current_pixel] ^= 0xFFFFFF;
+        }
+    }
+    
+    output_tx.send(buffer.clone()).unwrap();
 }
 
 const FONT_START: usize = 0x50;
@@ -79,13 +163,13 @@ fn load_fonts(memory: &mut Memory) {
 
 const PROGRAM_START: usize = 0x200;
 
-fn load_program(ram: &mut Memory, mut rom: impl std::io::Read) {
+fn load_program(memory: &mut Memory, mut rom: impl std::io::Read) {
     let mut buffer = Vec::new();
     rom.read_to_end(&mut buffer).expect("Failed to read ROM");
 
     let start = PROGRAM_START;
-    let end = PROGRAM_START + buffer.len().min(ram.len() - PROGRAM_START);
-    ram[start..end].copy_from_slice(&buffer[..(end - start)]);
+    let end = PROGRAM_START + buffer.len().min(memory.len() - PROGRAM_START);
+    memory[start..end].copy_from_slice(&buffer[..(end - start)]);
 }
 
 #[cfg(test)]
