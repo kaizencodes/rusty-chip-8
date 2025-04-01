cd82ea19f0c039d3e8f617075240be76c7c11ef7
Daniel Szatmari <daniel.kaizencodes@gmail.com>
2025-04-01 22:50:01 +0100

    refactor audio

---
+++ src/emulator/audio_handler.rs
@@ -0,0 +1,24 @@
+use rodio::{source::SineWave, OutputStream, Sink, Source};
+
+pub struct AudioHandler {
+    track: Sink
+}
+
+impl AudioHandler {
+    pub fn init() -> Self {
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        let beep = SineWave::new(440.0).amplify(0.2).repeat_infinite();
+        sink.append(beep.clone());  
+
+        Self { track: sink }
+    }
+
+    pub fn call(&self, timer: u8) {
+        if timer > 0 as u8 {
+            self.track.play();
+        } else {
+            self.track.pause();
+        }
+    }
+}
